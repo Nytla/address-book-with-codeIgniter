778ba308a1a1d44c08647e09e62cacda9100869a
nytla <nytla@mail.ru>
2013-01-26 20:44:41 +0200

    Update application/language/english/locale_lang.php

--- application/language/english/locale_lang.php
+++ application/language/english/locale_lang.php
@@ -1,7 +1,22 @@
 <?php
-$lang['site_name'] = 'Address Book';
-
-
-
-
+/**
+ * 
+ */
+$lang = array(
+  'english' => array(
+	
+		'site' => array(
+			'name' => 'Address Book',
+			'year' => 2012
+		),
+		
+		'book_list' => array(
+			'title' 		=> 'Public Area',
+			'search_word'	=> 'Search',
+			'keywords_word'	=> 'Keywords',
+			'country_word'	=> 'Country',
+			'city_word'		=> 'City'
+		)
+	)
+);
 ?>
