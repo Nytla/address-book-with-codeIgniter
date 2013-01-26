6968d3ada47fb71e757cf2a2ffef79562b2dc8d4
nytla <nytla@mail.ru>
2013-01-26 20:45:11 +0200

    Update application/controllers/booklist.php

--- application/controllers/booklist.php
+++ application/controllers/booklist.php
@@ -22,12 +22,60 @@ class Booklist extends CI_Controller {
 		/**
 		 * Set data for template
 		 */
+		$this->config->load('config', TRUE);
+		
+		$config = $this->config->item('config');
+		
+		//$config['common_charset'];
+		
+		//$config['paths']['root'];
+		
 		$data['title'] = 'First Load Page';
+		
 		$data['cont'] = 'This ... is test ^).' . $this -> testFunc();
 
 		$this -> load -> library('pageforming');
 		
-		$data['forming'] = $this -> pageforming -> loadTest();
+//		$data['forming'] = $this -> pageforming -> loadTest();
+		
+		/**
+		 * 
+		 */
+		$this -> pageforming -> headerCreate('book_list', TRUE);
+		
+		/**
+		 * CSS
+		 */
+		$demo_table = array(
+			'file' => 'demo_table.css'
+		);
+
+		$jquery_ui = array(
+			'file' => 'jquery_ui.css'
+		);
+		
+		$css_array = array(
+			$demo_table,
+			$jquery_ui
+		);
+		
+		/**
+		 * JS
+		 */
+		$jquery_library = array(
+			'file' => 'jquery_1.8.0.js'
+		);
+
+		$book_list = array(
+			'file' => 'book_list.js'
+		);
+		
+		$js_array = array(
+			$jquery_library,
+			$book_list
+		);
+		
+		$this -> pageforming -> scriptsCreate($css_array, $js_array);
 		
 		/**
 		 * Parse our template
